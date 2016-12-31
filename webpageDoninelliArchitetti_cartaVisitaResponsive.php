fda26e89f3d63ee9e12da30e6bc73eb738821cda
DonFran <francesco.doninelli@gmail.com>
2016-12-31 03:06:10 +0100

    Add files via upload

---
+++ webpageDoninelliArchitetti_cartaVisitaResponsive.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>DONINELLI ARCHITETTI</title>
+		<link rel="stylesheet" type="text/css" href="style_cartaVisitaResponsive.css">
+	</head>
+
+	<body>
+
+		<h1>DONINELLI ARCHITETTI</h1>
+
+		<div id="slider">
+			<figure>
+			<img id="firstImg" src="http://i64.tinypic.com/wwf887.jpg" align="middle";>
+			<img id="secondImg" src="http://i66.tinypic.com/igeo3k.jpg" align="middle";>
+			</figure>
+		</div>
+
+
+		<p>+41 91 825 54 75 | +41 79 621 35 90 | arch.doninelli@ticino.com | Piazza Grande 43, 6512 Giubiasco, CH | CHE - 102.027.068 IVA</p>
+
+
+
+	</body>
+	
+	
+</html>
